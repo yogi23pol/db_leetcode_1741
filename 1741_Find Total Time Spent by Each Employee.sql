b2cfac6a65653d233ee653f1195aa9e26baf9e86
Yogesh Pol <132032441+yogi23pol@users.noreply.github.com>
2024-07-02 10:53:26 +0530

    modified table name

--- 1741_Find Total Time Spent by Each Employee.sql
+++ 1741_Find Total Time Spent by Each Employee.sql
@@ -1,19 +1,19 @@
-Create table If Not Exists Employees_42(
+Create table If Not Exists Employees_43(
 emp_id int, 
 event_day date, 
 in_time int, 
 out_time int
 );
 
-Truncate table Employees_42;
+Truncate table Employees_43;
 
-insert into Employees_42 (emp_id, event_day, in_time, out_time) values (1, '2020-11-28', 4, 32);
-insert into Employees_42 (emp_id, event_day, in_time, out_time) values (1, '2020-11-28', 55, 200);
-insert into Employees_42 (emp_id, event_day, in_time, out_time) values (1, '2020-12-3', 1, 42);
-insert into Employees_42 (emp_id, event_day, in_time, out_time) values (2, '2020-11-28', 3, 33);
-insert into Employees_42 (emp_id, event_day, in_time, out_time) values (2, '2020-12-9', 47, 74);
+insert into Employees_43 (emp_id, event_day, in_time, out_time) values (1, '2020-11-28', 4, 32);
+insert into Employees_43 (emp_id, event_day, in_time, out_time) values (1, '2020-11-28', 55, 200);
+insert into Employees_43 (emp_id, event_day, in_time, out_time) values (1, '2020-12-3', 1, 42);
+insert into Employees_43 (emp_id, event_day, in_time, out_time) values (2, '2020-11-28', 3, 33);
+insert into Employees_43 (emp_id, event_day, in_time, out_time) values (2, '2020-12-9', 47, 74);
 
-SELECT * FROM Employees_42;
+SELECT * FROM Employees_43;
 
 /*
 Q. Write a solution to calculate the total time in minutes spent by each employee on each day at the office. Note that within one day, an employee can enter and leave more than once. The time spent in the office for a single entry is out_time - in_time.
@@ -46,7 +46,7 @@ Employee 2 has two events: one on day 2020-11-28 with a total of (33 - 3) = 30, 
 */
 
 SELECT event_day AS day, emp_id, SUM(out_time-in_time) AS total_time
-FROM Employees_42
+FROM Employees_43
 GROUP BY day, emp_id;
 
 
